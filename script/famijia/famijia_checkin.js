ed41678556156c6ab258a4a0ea34765c3cd2f120
blackmatrix7 <27717518+blackmatrix7@users.noreply.github.com>
2020-11-07 00:44:41 +0800

    Fa米家适配新版接口及去开屏广告

--- script/famijia/famijia_checkin.js
+++ script/famijia/famijia_checkin.js
@@ -1,5 +1,6 @@
 const scriptName = 'Fa米家';
-const getCookieRegex = /^https?:\/\/fmapp\.chinafamilymart\.com\.cn\/api\/app\/market\/member\/sign\/current/;
+const getCookieRegex = /^https?:\/\/fmapp\.chinafamilymart\.com\.cn\/api\/app\/market\/member\/signin\/usersign/;
+const startAdRegex = /^https?:\/\/fmapp\.chinafamilymart\.com\.cn\/api\/app\/market\/start\/ad/;
 const famijiaCookieKey = 'famijia_checkin_cookie';
 const famijiaDeviceIdKey = 'famijia_device_id_cookie';
 let magicJS = MagicJS(scriptName, "INFO");
@@ -80,12 +81,61 @@ function CheckIn(cookie, code, deviceId){
       }
       else{
         try{
+          magicJS.logDebug(`Fa米家签到响应结果：${data}`);
           let obj = typeof data === 'string'? JSON.parse(data) : data;
           if (obj.code === "200"){
             resolve([obj.data.todayRewardNum, obj.data.signCount, obj.data.resWordUp, obj.data.resWordDown, ""]);
           }
           else if (obj.code === "1000"){
             resolve([null, null, null, null, obj.message]);
+          }
+          else{
+            magicJS.logError(`签到失败，响应异常：${data}`);
+            reject('❌签到失败，响应异常，请查阅日志！');
+          }
+        }
+        catch(err){
+          magicJS.logError(`签到失败，执行异常：${err}，接口响应：${data}`);
+          reject('❌签到失败，执行异常，请查阅日志！');
+        }
+      }
+    })
+  })
+}
+
+function CheckInNewVerrsion(cookie, code, deviceId){
+  return new Promise((resolve, reject)=>{
+    let checkinOptions = {
+      url: 'https://fmapp.chinafamilymart.com.cn/api/app/market/member/signin/sign',
+      headers: {
+        "Accept": "*/*", 
+        "Accept-Encoding": "br;q=1.0, gzip;q=0.9, deflate;q=0.8",
+        "Accept-Language": "zh-Hans-CN;q=1.0",
+        "Connection": "keep-alive",
+        "Content-Type": "application/json",
+        "Host": "fmapp.chinafamilymart.com.cn",
+        "User-Agent": "Fa",
+        "deviceId": deviceId,
+        "fmVersion": "1.2.3",
+        "loginChannel": "app",
+        "token": cookie
+      },
+      body: {}
+    }
+    magicJS.post(checkinOptions, (err, resp, data)=>{
+      if (err){
+        magicJS.logError(`签到失败，请求异常：${err}`);
+        reject('❌签到失败，请求异常，请查阅日志！');
+      }
+      else{
+        try{
+          magicJS.logDebug(`Fa米家签到响应结果：${data}`);
+          let obj = typeof data === 'string'? JSON.parse(data) : data;
+          if (obj.code === "200"){
+            resolve([obj.data, ""]);
+          }
+          else if (obj.code === "3004000"){
+            resolve([null, obj.message]);
           }
           else{
             magicJS.logError(`签到失败，响应异常：${data}`);
@@ -160,6 +210,19 @@ function GetMili(cookie, deviceId){
       magicJS.logInfo('Cookie没有变化，无需更新');
     }
   }
+  else if (magicJS.isResponse && startAdRegex.test(magicJS.request.url)){
+    try{
+      let obj = JSON.parse(magicJS.response.body);
+      if (obj.code === '200'){
+        obj.data.relayDisplayUrl = '';
+      }
+      let body = JSON.stringify(obj);
+      magicJS.done({body});
+    }
+    catch (err){
+      magicJS.logWarning(`去除开屏广告失败，异常信息：${err}`);
+    }
+  }
   else{
     let subTitle = "";
     let content = "";
@@ -170,25 +233,42 @@ function GetMili(cookie, deviceId){
       magicJS.notify('❓没有读取到有效Cookie，请先从App中获取Cookie!!');
     }
     else{
-      let [getCodeErr, signCode] =  await magicJS.attempt(magicJS.retry(GetSignCode, 3, 1000)(cookie, deviceId), "");
-      if (getCodeErr){
-        subTitle = getCodeErr;
+      // 2020.11.04 新版签到
+      let [checkInErr,[data, message]] = await magicJS.attempt(magicJS.retry(CheckInNewVerrsion, 3, 1000)(cookie, deviceId), []);
+      if (checkInErr){
+        subTitle = checkInErr;
+      }
+      else if (data){
+        subTitle = `本日签到成功`;
+        if (data.currentNumber !== null){
+          subTitle += `，共获得 ${data.currentNumber} 个Fa米粒。`
+        }
+        content = `再坚持签到 ${data.nextDay} 天可获得 ${data.nextNumber} 个Fa米粒。\n签到 ${data.lastDay} 可获得 ${data.lastNumber} 个Fa米粒。`;
       }
       else{
-        magicJS.log(`当前获取的SignCode：${signCode}`);
-        let [checkInErr,[todayRewardNum, signCount, resWordUp, resWordDown, message]] = await magicJS.attempt(magicJS.retry(CheckIn, 3, 1000)(cookie, signCode, deviceId), [null, null, null, null, ""]);
-        if (checkInErr){
-          subTitle = checkInErr;
-        }
-        else if (!!message){
-          subTitle = message;
-        }
-        else{
-          subTitle = `连续签到${signCount}天，本日获得${todayRewardNum}米粒`;
-          if (!!resWordUp) content = resWordUp;
-          if (!!resWordDown) content = resWordDown;
-        }
+        subTitle = message;
       }
+      // 旧版废弃
+      // let [getCodeErr, signCode] =  await magicJS.attempt(magicJS.retry(GetSignCode, 3, 1000)(cookie, deviceId), "");
+      // if (getCodeErr){
+      //   subTitle = getCodeErr;
+      // }
+      // else{
+      //   magicJS.log(`当前获取的SignCode：${signCode}`);
+      //   let [checkInErr,[todayRewardNum, signCount, resWordUp, resWordDown, message]] = await magicJS.attempt(magicJS.retry(CheckIn, 3, 1000)(cookie, signCode, deviceId), [null, null, null, null, ""]);
+      //   if (checkInErr){
+      //     subTitle = checkInErr;
+      //   }
+      //   else if (!!message){
+      //     subTitle = message;
+      //   }
+      //   else{
+      //     subTitle = `连续签到${signCount}天，本日获得${todayRewardNum}米粒`;
+      //     if (!!resWordUp) content = resWordUp;
+      //     if (!!resWordDown) content = resWordDown;
+      //   }
+      // }
+
       // 获取米粒
       let [getMiliErr, getMiliResult] = await magicJS.attempt(GetMili(cookie, deviceId));
       if (!getMiliErr){
